7549fdc51875c3f32c3b648ad2ccb703e8b3e5e2
Victor Reyes <victor.reyes@hotmail.fr>
2021-06-08 08:59:42 +0200

    Add logrocket bug observer

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import LogRocket from "logrocket";
 
+LogRocket.init("v19f2f/vue3");
 createApp(App).use(store).use(router).mount("#app");
